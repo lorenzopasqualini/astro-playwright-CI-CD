09e900aac1db5823a9a7a90958c4f62fed7d123a
Lorenzo Pasqualini <lorenzopasqualini@outlook.com>
2023-08-25 20:24:57 +0200

    v0.7

--- e2e/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('example test', async ({ page }) => {
-	await page.goto('/'); // The baseURL here is the webServer URL
-	await expect(page).toHaveScreenshot();
-});
